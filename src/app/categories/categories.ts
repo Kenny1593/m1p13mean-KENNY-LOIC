834ff10479604dfe7234fd0ad02a02ce6e2c5c68
KennyRK <114820530+Kenny1593@users.noreply.github.com>
2026-02-28 13:32:30 +0300

    affichage catégories

--- src/app/categories/categories.ts
+++ src/app/categories/categories.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './categories.html',
   styleUrl: './categories.css',
 })
 export class Categories {
+
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 
 }
