c261f381c621cf0d7093eb197a51c20cd6fa0954
Labarre12 <labarreitokiana@gmail.com>
2026-03-01 03:04:38 +0300

    categorie

--- src/app/categories/categories.ts
+++ src/app/categories/categories.ts
@@ -3,16 +3,26 @@ import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { Footer } from '../footer/footer';
 import { Navbar } from '../navbar/navbar';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-categories',
-  imports: [RouterModule,Footer,Navbar],
+  imports: [RouterModule,Footer,Navbar,FormsModule,CommonModule],
   standalone: true,
   templateUrl: './categories.html',
   styleUrl: './categories.css',
 })
 export class Categories {
+  categorie = {
+    nom: '',
+    description: '',
+    image: '',
+    actif: true
+  };
 
+  onSubmit() {
+    console.log(this.categorie);
+  }
   
 
 }
