9befbfd23cea8e9d91168e96e3955f22c9c83b70
Labarre12 <labarreitokiana@gmail.com>
2026-03-03 17:50:12 +0300

    navboutique

---
+++ src/app/navboutique/navboutique.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterModule],
+  standalone: true,
+  templateUrl: './navboutique.html',
+  styleUrl: './navboutique.css',
+})
+export class Navboutique {
+
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+}
